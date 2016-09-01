07184f6da091f52158da3928f01db461c61e3068
WitheredFlame <witheredspam@gmail.com>
2016-09-01 18:33:41 -0500

    BAT Example

---
+++ Back up Everything.bat
@@ -0,0 +1,26 @@
+
+
+DEL C:\Users\FireFlame682176\Desktop\Space_engineers\BackUpsSC7END\
+
+XCOPY C:\Users\FireFlame682176\Desktop\Space_engineers\BackUpsSC7ALT C:\Users\FireFlame682176\Desktop\Space_engineers\BackUpsSC7END /C /S /D /Y /I
+
+DEL C:\Users\FireFlame682176\Desktop\Space_engineers\BackUpsSC7ALT\
+
+XCOPY C:\Users\FireFlame682176\Desktop\Space_engineers\BackUpsSC7 C:\Users\FireFlame682176\Desktop\Space_engineers\BackUpsSC7ALT /C /S /D /Y /I
+
+DEL C:\Users\FireFlame682176\Desktop\Space_engineers\BackUpsSC7\
+
+XCOPY C:\Users\FireFlame682176\AppData\Roaming\SpaceEngineers\Saves\76561198082806956\NovaSkys C:\Users\FireFlame682176\Desktop\Space_engineers\BackUpsSC7 /C /S /D /Y /I
+
+
+
+
+
+echo Done!
+echo Done!
+echo Done!
+echo Done!
+echo Done!
+echo Done!
+TimeOut 3
+
